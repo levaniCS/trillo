b4f20723f5d2a657c736b62d9e1261cfc346b2ea
levaniCS <levanisarishvili3105@gmail.com>
2020-03-06 21:30:39 +0400

    Updates

---
+++ precache-manifest.1e7709e6f6da479404f77d42fdc9a2a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb992262674cea4f7bc3f8cb42b10be5",
+    "url": "/trillo/index.html"
+  },
+  {
+    "revision": "ddc51633f8c6e4f5050e",
+    "url": "/trillo/static/css/main.00e44987.chunk.css"
+  },
+  {
+    "revision": "7a6840bcbb067a24d1b2",
+    "url": "/trillo/static/js/2.ee266a61.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/trillo/static/js/2.ee266a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc51633f8c6e4f5050e",
+    "url": "/trillo/static/js/main.be12079d.chunk.js"
+  },
+  {
+    "revision": "f6ec6d6733223369c518",
+    "url": "/trillo/static/js/runtime-main.db81a2c3.js"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/trillo/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillo/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillo/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillo/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "0de270de893aa7852ddc08775d6391b8",
+    "url": "/trillo/static/media/sprite.0de270de.svg"
+  },
+  {
+    "revision": "ccbe01b07ebcf2b6b331a3e82f64e970",
+    "url": "/trillo/static/media/user.ccbe01b0.jpeg"
+  }
+]);
